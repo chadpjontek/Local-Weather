8926663146e2b66aaefdf0c06e6bebc06b9cb301
Chad Pjontek <chadpjontek@gmail.com>
2017-10-06 06:38:26 -0400

    Mariahji completed. Added 5 day forcast, fixed toggle button, style changes, and code refactored.

--- Local-Weather.js
+++ Local-Weather.js
@@ -1,11 +1,11 @@
+// set global vars
+var lat = 0;
+var lon = 0;
+var isCelsius = false;
+var endPoint = "";
+var temp = [];
+var weatherId = 800;
 $(document).ready(function () {
-  // set global vars
-  var lat = 0;
-  var lon = 0;
-  var isCelsius = false;
-  var endPoint = "";
-  var temp = 0;
-  var weatherId = 800;
   //get users location
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -18,15 +18,102 @@ $(document).ready(function () {
         "&lon=" +
         lon +
         "&APPID=33e85e38014263a2ba46a9b66353c007";
-      //format & display json
       $.getJSON(endPoint, function (json) {
-        //show current weather condition
-        $("#weather").html(json.list[0].weather[0].description);
-        //show current temp
-        getTemp(json.list[0].main.temp);
         //display current city
         $("#city").html(json.city.name);
-        //function to get weather id
+        //show temps
+        for (var i = 0; i < json.list.length; i++) {
+          temp.push(json.list[i].main.temp)
+        };
+        for (var i = 0; i < temp.length; i++) {
+          $("#temp" + i).html(getTemp(temp[i]) + "&degF");
+        };
+        //show days/hours
+        for (var i = 1; i < json.list.length; i++) {
+          var day = dayOfWeek(json.list[i].dt);
+          var hour = hourOfDay(json.list[i].dt);
+          if (i > 6) {
+            $("#hour" + i).html(day);
+          } else {
+            $("#hour" + i).html(day + " " + hour);
+          }
+         
+        };
+        //weather conditions and images
+        for (var i = 0; i < json.list.length; i++) {
+          //show condition
+          $("#weather" + i).html(json.list[i].weather[0].description);
+          //get weatherIDs and display appropriate imgs
+          //DEV NOTE -- you can switch bitmojis using different numbers. See https://github.com/JoshCheek/bitmoji 
+          weatherId = getWeatherId(i);
+          switch (weatherId) {
+            case "thunderstorms":
+              $("#img" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/10222610-120048733_6-s1-v1.png?transparent=1&palette=1");
+              break;
+            case "drizzle":
+              $("#img" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/9988646-120048733_6-s1-v1.png?transparent=1&palette=1");
+              break;
+            case "rain":
+              $("#img" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/10212130-120048733_6-s1-v1.png?transparent=1&palette=1");
+              break;
+            case "snow":
+              $("#img" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/7834033-120048733_6-s1-v1.png?transparent=1&palette=1");
+              break;
+            case "fog":
+              $("#img" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/10214712-120048733_6-s1-v1.png?transparent=1&palette=1");
+              break;
+            case "clear":
+              $("#img" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/9988986-120048733_6-s1-v1.png?transparent=1&palette=1");
+              break;
+            case "clouds":
+              $("#img" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/10221429-120048733_6-s1-v1.png?transparent=1&palette=1");
+              break;
+            case "extreme":
+              $("#img" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/10134192-120048733_6-s1-v1.png?transparent=1&palette=1");
+              break;
+            case "wind":
+              $("#img" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/10217325-120048733_6-s1-v1.png?transparent=1&palette=1");
+              break;
+            case "hot":
+              $("#img" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/10192284-120048733_6-s1-v1.png?transparent=1&palette=1");
+              break;
+          };
+        };
+        //change color scheme for current weather
+        weatherId = getWeatherId(0);
+        switch (weatherId) {
+          case "thunderstorms":
+            $("body").css("background", "linear-gradient(to left top, #0E476F, #60DBFC)");
+            break;
+          case "drizzle":
+            $("body").css("background", "linear-gradient(to left top, #214081, #d72568)");
+            break;
+          case "rain":
+            $("body").css("background", "linear-gradient(to left top, #a5d3e9, #6e628a)");
+            break;
+          case "snow":
+            $("body").css("background", "linear-gradient(to left top, #c9ecec, #25a6a6)");
+            break;
+          case "fog":
+            $("body").css("background", "#linear-gradient(to left top, #c4946e, #262626)");
+            break;
+          case "clear":
+            $("body").css("background", "linear-gradient(to left top, #ffb227, #a8ffed)");
+            break;
+          case "clouds":
+            $("body").css("background", "linear-gradient(to left top, #FFE2F4, #A2D6FA)");
+            break;
+          case "extreme":
+            $("body").css("background", "linear-gradient(to left top, #fefefe, #800706)");
+            break
+          case "wind":
+            $("body").css("background", "linear-gradient(to left top, #6a5e87, #bbc4c9)");
+            break;
+          case "hot":
+            $("body").css("background", "linear-gradient(to left top, #29c0e9, #706184)");
+            break;
+        };
+        //function to get weatherId
         function getWeatherId(i) {
           weatherId = json.list[i].weather[0].id;
           //lookup catagory with weatherId
@@ -63,151 +150,79 @@ $(document).ready(function () {
           };
           return weatherId;
         };
-        //get current weatherId
-        weatherId = getWeatherId(0);
-        //change img and color scheme based on weatherId
-        switch (weatherId) {
-          case "thunderstorms":
-            $("body").css("background", "linear-gradient(to left top, #0E476F, #60DBFC)");
-            $("#img").attr("src", "https://render.bitstrips.com/v2/cpanel/10222610-120048733_6-s1-v1.png?transparent=1&palette=1");
-            break;
-          case "drizzle":
-            $("body").css("background", "linear-gradient(to left top, #214081, #d72568)");
-            $("#img").attr("src", "https://render.bitstrips.com/v2/cpanel/9988646-120048733_6-s1-v1.png?transparent=1&palette=1");
-            break;
-          case "rain":
-            $("body").css("background", "linear-gradient(to left top, #a5d3e9, #6e628a)");
-            $("#img").attr("src", "https://render.bitstrips.com/v2/cpanel/10212130-120048733_6-s1-v1.png?transparent=1&palette=1");
-            break;
-          case "snow":
-            $("body").css("background", "linear-gradient(to left top, #c9ecec, #25a6a6)");
-            $("#img").attr("src", "https://render.bitstrips.com/v2/cpanel/7834033-120048733_6-s1-v1.png?transparent=1&palette=1");
-            break;
-          case "fog":
-            $("body").css("background", "#linear-gradient(to left top, #c4946e, #262626)");
-            $("#img").attr("src", "https://render.bitstrips.com/v2/cpanel/10214712-120048733_6-s1-v1.png?transparent=1&palette=1");
-            break;
-          case "clear":
-            $("body").css("background", "linear-gradient(to left top, rgb(255, 178, 39), #a8ffed)");
-            $("#img").attr("src", "https://render.bitstrips.com/v2/cpanel/9988986-120048733_6-s1-v1.png?transparent=1&palette=1");
-            break;
-          case "clouds":
-            $("body").css("background", "linear-gradient(to left top, #FFE2F4, #A2D6FA)");
-            $("#img").attr("src", "https://render.bitstrips.com/v2/cpanel/10221429-120048733_6-s1-v1.png?transparent=1&palette=1");
-            break;
-          case "extreme":
-            $("body").css("background", "linear-gradient(to left top, #fefefe, #800706)");
-            $("#img").attr("src", "https://render.bitstrips.com/v2/cpanel/10134192-120048733_6-s1-v1.png?transparent=1&palette=1");
-            break;
-          case "wind":
-            $("body").css("background", "linear-gradient(to left top, #6a5e87, #bbc4c9)");
-            $("#img").attr("src", "https://render.bitstrips.com/v2/cpanel/10217325-120048733_6-s1-v1.png?transparent=1&palette=1");
-            break;
-          case "hot":
-            $("body").css("background", "linear-gradient(to left top, #29c0e9, #706184)");
-            $("#img").attr("src", "https://render.bitstrips.com/v2/cpanel/10192284-120048733_6-s1-v1.png?transparent=1&palette=1");
-            break;
-        };
-        //loop through forcast and display
-        for (var i = 1; i <= json.list.length; i++) {
-          weatherId = getWeatherId(i);
-          switch (weatherId) {
-            case "thunderstorms":
-              $("#imgHourly" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/10222610-120048733_6-s1-v1.png?transparent=1&palette=1");
-              break;
-            case "drizzle":
-              $("#imgHourly" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/9988646-120048733_6-s1-v1.png?transparent=1&palette=1");
-              break;
-            case "rain":
-              $("#imgHourly" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/10212130-120048733_6-s1-v1.png?transparent=1&palette=1");
-              break;
-            case "snow":
-              $("#imgHourly" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/7834033-120048733_6-s1-v1.png?transparent=1&palette=1");
-              break;
-            case "fog":
-              $("#imgHourly" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/10214712-120048733_6-s1-v1.png?transparent=1&palette=1");
-              break;
-            case "clear":
-              $("#imgHourly" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/9988986-120048733_6-s1-v1.png?transparent=1&palette=1");
-              break;
-            case "clouds":
-              $("#imgHourly" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/10221429-120048733_6-s1-v1.png?transparent=1&palette=1");
-              break;
-            case "extreme":
-              $("#imgHourly" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/10134192-120048733_6-s1-v1.png?transparent=1&palette=1");
-              break;
-            case "wind":
-              $("#imgHourly" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/10217325-120048733_6-s1-v1.png?transparent=1&palette=1");
-              break;
-            case "hot":
-              $("#imgHourly" + i).attr("src", "https://render.bitstrips.com/v2/cpanel/10192284-120048733_6-s1-v1.png?transparent=1&palette=1");
-              break;
-          };
-          $("#hour" + i).html(dayOfWeek(json.list[i].dt));
-          $("#weatherHourly" + i).html(json.list[i].weather[0].description);
-          $("#tempHourly" + i).html(getTemp(json.list[i].main.temp));
-        };
       });
     });
   };
-  //toggle between Celsius & Fahrenheit
-  $("#toggle").on("click", function () {
-    isCelsius = !isCelsius;
-    if (isCelsius) {
-      $("#temp").html(Math.round(temp - 273.15) + "&degC");
-    } else {
-      $("#temp").html(Math.round(temp * 1.8 - 459.67) + "&degF");
-    }
-  });
-
-  //show current temp in either C or F
-  var getTemp = function (temp) {
-    if (isCelsius) {
-      return Math.round(temp - 273.15);
-    } else {
-      return Math.round(temp * 1.8 - 459.67);
+});
+//toggle between Celsius & Fahrenheit - loop through all temps
+$("#toggle").on("change", function () {
+  isCelsius = !isCelsius;
+  if (isCelsius) {
+    for (var i = 0; i < temp.length; i++) {
+      $("#temp" + i).html(Math.round(temp[i] - 273.15) + "&degC");
     };
-  }
-
-  //find day of week
-  var dayOfWeek = function (date) {
-    var xx = new Date();
-    xx.setTime(date * 1000); // javascript timestamps are in milliseconds
-    var day = xx.getDay(); // the Day
-    switch (day) {
-      case 0:
-        return "Sun";
-      case 1:
-        return "Mon";
-      case 2:
-        return "Tue";
-      case 3:
-        return "Wed";
-      case 4:
-        return "Thur";
-      case 5:
-        return "Fri";
-      case 6:
-        return "Sat";
+  } else {
+    for (var i = 0; i < temp.length; i++) {
+      $("#temp" + i).html(Math.round(temp[i] * 1.8 - 459.67) + "&degF");
     };
-  }
-
-  // Helper function for add element box list in WOW
-  WOW.prototype.addBox = function (element) {
-    this.boxes.push(element);
   };
+});
+//show current temp in either C or F
+var getTemp = function (temp) {
+  if (isCelsius) {
+    return Math.round(temp - 273.15);
+  } else {
+    return Math.round(temp * 1.8 - 459.67);
+  };
+}
+//find day of week
+var dayOfWeek = function (date) {
+  var xx = new Date();
+  xx.setTime(date * 1000); // javascript timestamps are in milliseconds
+  var day = xx.getDay(); // the Day
+  switch (day) {
+    case 0:
+      return "Sun";
+    case 1:
+      return "Mon";
+    case 2:
+      return "Tue";
+    case 3:
+      return "Wed";
+    case 4:
+      return "Thur";
+    case 5:
+      return "Fri";
+    case 6:
+      return "Sat";
+  };
+}
+//find hour in AM/PM
+var hourOfDay = function(timestamp){
+  var date = new Date(timestamp*1000);
+  var hours = date.getHours();
+  if(hours < 12){
+    return hours + "AM";
+  } else if (hours === 12) {
+    return hours + "PM";
+  } else {
+    return (hours -12) + "PM";
+  }
+}
 
-  // Init WOW.js and get instance
-  var wow = new WOW();
-  wow.init();
-
-  // Attach scrollSpy to .wow elements for detect view exit events,
-  // then reset elements and add again for animation
-  $('.wow').on('scrollSpy:exit', function () {
-    $(this).css({
-      'visibility': 'hidden',
-      'animation-name': 'none'
-    }).removeClass('animated');
-    wow.addBox(this);
-  }).scrollSpy();
-})
+// Helper function for add element box list in WOW
+WOW.prototype.addBox = function (element) {
+  this.boxes.push(element);
+};
+// Init WOW.js and get instance
+var wow = new WOW();
+wow.init();
+// Attach scrollSpy to .wow elements for detect view exit events,
+// then reset elements and add again for animation
+$('.wow').on('scrollSpy:exit', function () {
+  $(this).css({
+    'visibility': 'hidden',
+    'animation-name': 'none'
+  }).removeClass('animated');
+  wow.addBox(this);
+}).scrollSpy();
